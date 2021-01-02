f0a94b7c6f82770e48416476701af328f4b4e2d1
Kevin Kaichuang Yang <kevyan@microsoft.com>
2021-01-02 01:47:15 -0500

    Finish 2020 analysis.

--- analysis_2020.py
+++ analysis_2020.py
@@ -8,31 +8,36 @@ pal = sns.color_palette()
 
 
 df = pd.read_csv('books.csv')
-df = df[df['date'].apply(lambda x: str(x)[-2:] == '20')]
-demo = df[['race2', 'sex', 'Genre']]
-demo = demo.rename(columns={'sex': 'gender', 'race2': 'race', 'Genre': 'genre'})
+year_read = ['20' + str(x)[-2:] for x in df['date']]
+# year_read = [int(x) for x in year_read]
+df['year_read'] = year_read
+df = df[df['year_read'].isin(['2019', '2020'])]
+demo = df[['race2', 'sex', 'Genre', 'year_read']]
+demo = demo.rename(columns={'sex': 'gender', 'race2': 'race', 'Genre': 'genre', 'year_read': 'year read'})
 
 fig, axs = plt.subplots(1, 3, figsize=(12, 3), gridspec_kw={'width_ratios': [1, 1, 2]})
 for ax, x in zip(axs, ['gender', 'genre', 'race']):
-    a = sns.countplot(x=x, data=demo, ax=ax)
+    a = sns.countplot(x=x, data=demo, ax=ax, hue='year read')
+    if x != 'gender':
+        ax.get_legend().remove()
     for p in a.patches:
         height = p.get_height()
         if ~np.isnan(height):
             a.text(p.get_x() + p.get_width() / 2., height + 0.1, int(height), ha="center")
 _ = fig.tight_layout()
-_ = fig.savefig('demo2020.png', dpi=350)
+_ = fig.savefig('demo2020.png', dpi=350, bbox_inches='tight')
 
-graph = sns.catplot(x='race', hue='gender', col='genre', kind='count', data=demo)
+graph = sns.catplot(x='race', hue='gender', col='genre', kind='count', data=demo[demo['year read'] == '2020'])
 for a in graph.axes[0]:
     for p in a.patches:
         height = p.get_height()
         if ~np.isnan(height):
             a.text(p.get_x() + p.get_width() / 2., height + 0.1, int(height), ha="center")
-_ = plt.savefig('demo_facet2020.png', dpi=350)
+_ = plt.savefig('demo_facet2020.png', dpi=350, bbox_inches='tight')
 
-fig = plt.figure()
-graph = sns.histplot(data=df, x='year', discrete=True)
-a = graph.axes
+fig, ax = plt.subplots(1, 1)
+df = df[df['year_read'].isin(['2019', '2020'])]
+a = sns.histplot(data=df, x='year', hue='year_read', ax=ax, discrete=True, multiple='dodge')
 for p in a.patches:
     height = p.get_height()
     if height > 0.5:
